09b24cc1eaf338a05ff0bf8ed76c78685b09a4f8
DDC <not@nemail.com>
2011-08-29 16:41:39 -0400

    Finally got editor working, although its just a basic text editor for the moment

--- src/arduinoplugin/editors/PdeEditor.java
+++ src/arduinoplugin/editors/PdeEditor.java
@@ -1,59 +1,14 @@
 package arduinoplugin.editors;
 
-import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.swt.widgets.Composite;
-import org.eclipse.ui.IEditorInput;
-import org.eclipse.ui.IEditorSite;
-import org.eclipse.ui.PartInitException;
-import org.eclipse.ui.part.EditorPart;
+import org.eclipse.ui.editors.text.TextEditor;
 
-public class PDEEditor extends EditorPart {
+public class PDEEditor extends TextEditor {
 
 	public PDEEditor() {
-		// TODO Auto-generated constructor stub
+		super();
 	}
-
-	@Override
-	public void doSave(IProgressMonitor monitor) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void doSaveAs() {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void init(IEditorSite site, IEditorInput input)
-			throws PartInitException {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public boolean isDirty() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public boolean isSaveAsAllowed() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public void createPartControl(Composite parent) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void setFocus() {
-		// TODO Auto-generated method stub
-
+	public void dispose() {
+		super.dispose();
 	}
 
 }
