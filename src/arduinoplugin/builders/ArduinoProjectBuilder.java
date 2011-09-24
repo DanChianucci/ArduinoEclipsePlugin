9f3d71ef88711c359814d9aba04212ead0e0c6f9
DDC <not@nemail.com>
2011-09-24 17:48:10 -0400

    cleaned up unneeded commented code

--- src/arduinoplugin/builders/ArduinoProjectBuilder.java
+++ src/arduinoplugin/builders/ArduinoProjectBuilder.java
@@ -64,32 +64,11 @@ public class ArduinoProjectBuilder extends IncrementalProjectBuilder {
 		}
 
 		return null;
-
-		/*
-		 * if (kind == IncrementalProjectBuilder.FULL_BUILD) {
-		 * fullBuild(monitor); } else { IResourceDelta delta =
-		 * getDelta(getProject()); if (delta == null) { fullBuild(monitor); }
-		 * else { incrementalBuild(delta, monitor); } } return null;
-		 */
 	}
 
 	protected void clean(IProgressMonitor monitor) throws CoreException 
 	{
 		PluginBase.removeDescendants(new File(getOutputPath()));
 	}
-
-	/*
-	 * private void incrementalBuild(IResourceDelta delta, IProgressMonitor
-	 * monitor) { System.out.println("incremental build on " + delta); try {
-	 * delta.accept(new IResourceDeltaVisitor() { public boolean
-	 * visit(IResourceDelta delta) { System.out.println("changed: " +
-	 * delta.getResource().getRawLocation()); return true; // visit children too
-	 * } }); } catch (CoreException e) { e.printStackTrace(); }
-	 * 
-	 * }
-	 * 
-	 * private void fullBuild(IProgressMonitor monitor) {
-	 * System.out.println("Full Build"); }
-	 */
 
 }
