b6caa43c66f23ebe91067bc8b91815bcf683eeff
DDC <not@nemail.com>
2011-09-24 17:46:15 -0400

    new toolbar button to allow uploading

---
+++ src/arduinoplugin/Toolbars/DropDownHandler.java
@@ -0,0 +1,67 @@
+package arduinoplugin.Toolbars;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
+
+import arduinoplugin.Preprocess.Sketch;
+import arduinoplugin.builders.RunnerException;
+import arduinoplugin.natures.ArduinoProjectNature;
+import arduinoplugin.uploader.Serial;
+import arduinoplugin.uploader.SerialException;
+
+public class DropDownHandler extends AbstractHandler {
+  private static final String PARM_MSG = "arduinoplugin.Toolbar.dropdownmsg";
+
+  public Object execute(ExecutionEvent event) throws ExecutionException 
+  {
+    String msg = event.getParameter(PARM_MSG);
+    if (msg == null) {
+      System.out.println("Error");
+    } 
+    else if(msg.equals("UploadCommand"))
+    {
+    	IProject projectToUpload;
+		projectToUpload = selectProject();
+    	String primaryClass = projectToUpload.getName()+".pde";
+    	String buildPath = projectToUpload.getLocation().toOSString()+File.separator+"bin";
+    	try {
+			Sketch s = new Sketch();
+			s.upload(buildPath, primaryClass, true, projectToUpload);
+		} catch (RunnerException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SerialException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}    	
+    }
+    return null;
+  }
+  
+  private IProject selectProject()
+  {
+	  //make a popup that lists all the projects with arduino nature
+	  IProject[] projects = ResourcesPlugin.getWorkspace().getRoot().getProjects();
+	  List<IProject> arduinoProjects = new ArrayList<IProject>();
+	  for(IProject p :projects)
+	  {
+		  try {
+			if(p.hasNature(ArduinoProjectNature.NATURE_ID));
+		} catch (CoreException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		  	arduinoProjects.add(p);
+	  }
+
+	return arduinoProjects.get(0);
+  }
+}
